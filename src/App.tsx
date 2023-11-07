c9d7f194f32b11afad3a288c58e4300442b403da
Enitan <enitanolawale0@gmail.com>
2023-11-07 14:19:00 +0100

    changed the api input div placeholder to an actual input element

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export const App = () => {
         <div className="w-fit flex flex-row mt-5">
           <p className="text-[2.5rem]">Вставьте ваш API</p>
           <div className="border border-x-4 border-[#74da7f] rounded-2xl py-4 px-2 mx-2 text-[1.25rem]">
-            rtps....текстовое поле ссылки
+            <input className="outline-none" type="text" placeholder="rtps....текстовое поле ссылки" />
           </div>
           <button className="bg-[#74da7f] px-2 rounded-2xl text-[2.25rem]">
             Добавить
